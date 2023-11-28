91c04190961bfd23ace483755ea6406a8bcf4f04
Daniel Souza Affonso Ferreira <daniel.souza@ebanx.com>
2023-11-28 14:32:59 -0300

    rendering only an icon inside a wrapper

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -11,9 +11,38 @@ const IconInput = ({
   icon,
   width = 250,
   size,
-  placeholder,
+  ...delegated
 }) => {
-  return 'TODO';
+  return (
+  <Wrapper>
+    <VisuallyHidden>{label}</VisuallyHidden>
+    <IconWrapper style={{ '--size': 16 + 'px' }}>
+      <Icon id={icon} size={16} />
+    </IconWrapper>
+    <TextInput {...delegated}/>
+  </Wrapper>
+  )
 };
+
+const Wrapper = styled.label`
+   display: block;
+   position: relative;
+`;
+
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`
+
+const TextInput = styled.input`
+  height: ${24 / 16}rem;
+  border: none;
+  border-bottom: 1px solid ${COLORS.black};
+  padding-left: 24px;
+`;
 
 export default IconInput;
